b1f09622084e65b941c3ebe58af458ad8182eed7
amanmishra75way <amishra.75way@gmail.com>
2025-07-02 13:57:10 +0530

    using optional props in typecript

--- src/Component.tsx
+++ src/Component.tsx
@@ -2,12 +2,13 @@ import React from "react";
 
 interface Component {
   text: string;
-  onClick: () => void;
+  onClick?: () => void; // adding this question mark will make the fn optional to use
 }
 const Component: React.FC<Component> = (prop) => {
   return (
     <div>
       <button onClick={prop.onClick}>{prop.text}</button>
+      <button>{prop.text}</button>
     </div>
   );
 };
