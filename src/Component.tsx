ba3ffca23406f55e383b2756e431a98854c654a5
amanmishra75way <amishra.75way@gmail.com>
2025-07-02 14:21:32 +0530

    Creating our own type in typescript

--- src/Component.tsx
+++ src/Component.tsx
@@ -4,10 +4,13 @@ interface Component {
   text: string;
   onClick?: () => void; // adding this question mark will make the fn optional to use
 }
-const Component: React.FC<Component> = (prop) => {
+//creating our own type
+type prop = { text: string; num: number };
+
+const Component: React.FC<prop> = (prop) => {
   return (
     <div>
-      <button onClick={prop.onClick}>{prop.text}</button>
+      {/* <button onClick={prop.onClick}>{prop.text}</button> */}
       <button>{prop.text}</button>
     </div>
   );
