59471e7d29fe667c94b53a9fe2013bb434feb523
amanmishra75way <amishra.75way@gmail.com>
2025-07-08 13:16:24 +0530

    Add User type definition and create a new user object

---
+++ readonlyOptional.ts
@@ -0,0 +1,20 @@
+type User = {
+  readonly _id: string;
+  name: string;
+  email: string;
+  mobile: number;
+  password: string;
+  isAdmin: true;
+};
+
+// creating a user object of User type which acess all value of user
+const newUser: User = {
+  _id: "1",
+  name: "Aman",
+  email: "amishra75way@gmail.com",
+  mobile: 123456789,
+  password: "12345sdb",
+  isAdmin: true,
+};
+
+newUser._id = "2"; // this can not be chabngebale beacuase readonly
