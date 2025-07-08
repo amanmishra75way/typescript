564fafd2d0ccffdb3025989364a32cc57eb7152c
amanmishra75way <amishra.75way@gmail.com>
2025-07-08 12:26:00 +0530

    Add email property to user object and implement userDetail function

--- Objects.js
+++ Objects.js
@@ -4,6 +4,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var user = {
     name: "Aman",
     age: 21,
-    email: "aman@gmail.com",
     isAdmin: true,
 };
+//passing object as parameter can pass 1type only once
+function regsiterUser(_a) {
+    var string = _a.name, number = _a.age, boolean = _a.isAdmin;
+}
+regsiterUser({ name: "Aman", age: 21, isAdmin: true });
+//function returning an object type
+function userDetail() {
+    return { name: "Aman", age: 21, isAdmin: true };
+}
--- Objects.ts
+++ Objects.ts
@@ -8,4 +8,10 @@ const user = {
 //passing object as parameter can pass 1type only once
 function regsiterUser({ name: string, age: number, isAdmin: boolean }) {}
 regsiterUser({ name: "Aman", age: 21, isAdmin: true });
+
+//function returning an object type
+function userDetail(): { name: string; age: number; isAdmin: true } {
+  return { name: "Aman", age: 21, isAdmin: true };
+}
+
 export {};
