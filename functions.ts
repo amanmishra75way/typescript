dfe5358191364de54915a9d02082a1ff79dc33b8
amanmishra75way <amishra.75way@gmail.com>
2025-07-08 11:02:53 +0530

    Add initial implementation of addTwo function with number and string input handling

---
+++ functions.ts
@@ -0,0 +1,6 @@
+function addTwo(num) {
+  return num + 2;
+}
+const result = addTwo(5); // this is passing a number
+const output = addTwo("Aman"); // this is passing a string, which should cause issuee to solve this
+console.log(result);
