def7ed0e52d6d9b183aa96b1530743edf76eb02f
amanmishra75way <amishra.75way@gmail.com>
2025-07-08 10:37:43 +0530

    Add type declarations for variables in TypeScript file

--- variables.js
+++ variables.js
@@ -1,4 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+//declaring string type variable
 var name = "Aman Mishra";
 console.log("Hello, ".concat(name, "!"));
+//declaring number type variable
+var age = 21;
+console.log("You are ".concat(age, " years old."));
--- variables.ts
+++ variables.ts
@@ -1,5 +1,14 @@
+//declaring string type variable
 let name: string = "Aman Mishra";
 console.log(`Hello, ${name}!`);
+
+//declaring number type variable
+let age: number = 21;
+console.log(`You are ${age} years old.`);
+
+//declaring boolean type variable
+let isStudent: boolean = true;
+console.log(`Are you a student? ${isStudent}`);
 
 //This line to remove the error
 export {};
